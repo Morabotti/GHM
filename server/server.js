878bd17e77ea5d4daadb51236e76765acb432f61
Jesse Batman <jesse.batman@outlook.com>
2019-02-08 23:08:51 +0200

    Added express server for GSI

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const express = require('express');
+const fallback = require('express-history-api-fallback')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('../front/build'));
+app.use(express.json());
+
+app.use(fallback('index.html', { root: __dirname + '../front/build' }))
+
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () => {
+  console.log("Listening port: " + PORT)
+});
