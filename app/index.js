13600d4f7b2332685b2b49926bd3cf927076d208
Jesse Batman <jesse.batman@outlook.com>
2019-05-21 15:17:26 +0300

    Added configuration options to app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
 const electron = require('electron')
+const fs = require('fs')
 const { app, BrowserWindow, globalShortcut } = require('electron')
-require('electron-reload')(__dirname);
+require('electron-reload')(__dirname)
 
 // TODO: Add support for Native functions
+const getConfig = async () => fs.readFileSync('config.json')
 
-app.on('ready', () => {
+const defConfig = {
+  controlRefresh: 'Alt+R',
+  controlQuit: 'Alt+P',
+  controlHide: 'Alt+C',
+  overlayUrl: 'http://localhost:8080/overlay'
+}
+
+app.on('ready', async () => {
+  let OVERLAY_URL,
+    CONTROLS_REFRESH,
+    CONTROLS_QUIT,
+    CONTROLS_HIDE
+
+  try {
+    const raw = await getConfig()
+    const config = JSON.parse(raw)
+    OVERLAY_URL = config.url !== undefined ? config.url : defConfig.overlayUrl
+    CONTROLS_REFRESH = config.controls.refresh !== undefined ? config.controls.refresh : defConfig.controlRefresh
+    CONTROLS_QUIT = config.controls.quit !== undefined ? config.controls.quit : defConfig.controlQuit
+    CONTROLS_HIDE = config.controls.hide !== undefined ? config.controls.hide : defConfig.controlHide
+  } catch (e) {
+    console.log('No config found! Create config.json into /app folder.')
+    console.log('Loading default configs.')
+    OVERLAY_URL = defConfig.overlayUrl
+    CONTROLS_REFRESH = defConfig.controlRefresh
+    CONTROLS_QUIT = defConfig.controlQuit
+    CONTROLS_HIDE = defConfig.controlHide
+  }
+
   const displays = electron.screen.getAllDisplays()
   const externalDisplay = displays.find((display) => {
     return display.bounds.x !== 0 || display.bounds.y !== 0
@@ -25,7 +55,7 @@ app.on('ready', () => {
     win.setIgnoreMouseEvents(true)
 
     // TOGGLE HUD
-    globalShortcut.register('Alt+C', () => {
+    globalShortcut.register(CONTROLS_REFRESH, () => {
       const isVisible = win.isVisible()
       if (isVisible)
         win.minimize()
@@ -34,15 +64,15 @@ app.on('ready', () => {
     })
 
     // EXIT PROGRAM
-    globalShortcut.register('Alt+P', () => {
+    globalShortcut.register(CONTROLS_QUIT, () => {
       win.close()
     })
 
     // REFRESH OVERLAY
-    globalShortcut.register('Alt+R', () => {
+    globalShortcut.register(CONTROLS_REFRESH, () => {
       win.reload()
     })
 
-    win.loadURL('http://localhost:8080/overlay')
+    win.loadURL(OVERLAY_URL)
   }
 })
