69a417a7868b6918472bedfd837e2ebc03050fa2
Jesse Batman <jesse.batman@outlook.com>
2019-05-31 23:00:43 +0300

    Minor changes to observer app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,8 @@
 const electron = require('electron')
 const fs = require('fs')
 const { app, BrowserWindow, globalShortcut } = require('electron')
+const config = require('electron-json-config');
 require('electron-reload')(__dirname)
-
-// TODO: Add support for Native functions
-const getConfig = async () => fs.readFileSync('config.json')
 
 const defConfig = {
   controlRefresh: 'Alt+R',
@@ -14,26 +12,12 @@ const defConfig = {
 }
 
 app.on('ready', async () => {
-  let OVERLAY_URL,
-    CONTROLS_REFRESH,
-    CONTROLS_QUIT,
-    CONTROLS_HIDE
+  console.log('Settings: ' + config.file())
 
-  try {
-    const raw = await getConfig()
-    const config = JSON.parse(raw)
-    OVERLAY_URL = config.url !== undefined ? config.url : defConfig.overlayUrl
-    CONTROLS_REFRESH = config.controls.refresh !== undefined ? config.controls.refresh : defConfig.controlRefresh
-    CONTROLS_QUIT = config.controls.quit !== undefined ? config.controls.quit : defConfig.controlQuit
-    CONTROLS_HIDE = config.controls.hide !== undefined ? config.controls.hide : defConfig.controlHide
-  } catch (e) {
-    console.log('No config found! Create config.json into /app folder.')
-    console.log('Loading default configs.')
-    OVERLAY_URL = defConfig.overlayUrl
-    CONTROLS_REFRESH = defConfig.controlRefresh
-    CONTROLS_QUIT = defConfig.controlQuit
-    CONTROLS_HIDE = defConfig.controlHide
-  }
+  if (!config.has('overlayUrl')) config.set('overlayUrl', defConfig.overlayUrl)
+  if (!config.has('controlRefresh')) config.set('controlRefresh', defConfig.controlRefresh)
+  if (!config.has('controlHide')) config.set('controlHide', defConfig.controlHide)
+  if (!config.has('controlQuit')) config.set('controlQuit', defConfig.controlQuit)
 
   const displays = electron.screen.getAllDisplays()
   const externalDisplay = displays.find((display) => {
@@ -55,7 +39,7 @@ app.on('ready', async () => {
     win.setIgnoreMouseEvents(true)
 
     // TOGGLE HUD
-    globalShortcut.register(CONTROLS_REFRESH, () => {
+    globalShortcut.register(config.get('controlHide', defConfig.controlHide), () => {
       const isVisible = win.isVisible()
       if (isVisible)
         win.minimize()
@@ -64,15 +48,15 @@ app.on('ready', async () => {
     })
 
     // EXIT PROGRAM
-    globalShortcut.register(CONTROLS_QUIT, () => {
+    globalShortcut.register(config.get('controlQuit', defConfig.controlQuit), () => {
       win.close()
     })
 
     // REFRESH OVERLAY
-    globalShortcut.register(CONTROLS_REFRESH, () => {
+    globalShortcut.register(config.get('controlRefresh', defConfig.controlRefresh), () => {
       win.reload()
     })
 
-    win.loadURL(OVERLAY_URL)
+    win.loadURL(config.get('overlayUrl', defConfig.overlayUrl))
   }
 })
