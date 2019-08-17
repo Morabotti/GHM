b7c24ffda9b6d2a66727d1dd56b77204df5b7b13
Jesse Batman <jesse.batman@outlook.com>
2019-08-17 21:02:08 +0300

    Fixed production webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
   },
   output: {
     filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/'
   },
   module: {
     rules: [
